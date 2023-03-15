99de89052cad0319b928dde674f83070b7af34fe
Cameron Book <43379611+ulmononian@users.noreply.github.com>
2023-03-15 15:52:48 -0700

    Update landda_orion.intel.lua

--- modulefiles/landda_orion.intel.lua
+++ modulefiles/landda_orion.intel.lua
@@ -5,7 +5,6 @@ loads Land DA prerequisites for Orion/Intel
 prepend_path("MODULEPATH", "/work/noaa/da/role-da/spack-stack/modulefiles")
 load("miniconda/3.9.7")
 
---prepend_path("MODULEPATH", pathJoin(os.getenv("EPICHOME"),"spack-stack/envs/landda-release-1.0-intel/install/modulefiles/Core"))
 prepend_path("MODULEPATH", "/work2/noaa/da/role-da/spack-stack-feature-r2d2-mysql/envs/unified-4.0.0-rc1/install/modulefiles/Core")
 
 load("stack-intel")
@@ -16,8 +15,6 @@ load("parallel-netcdf")
 load("cmake")
 load("ecbuild")
 load("stack-python")
-load("py-numpy")
-load("py-netcdf4")
 
 setenv("CC", "mpiicc")
 setenv("CXX", "mpiicpc")
